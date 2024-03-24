c789d19c65f83a6419051c6cbb8781d177883e9d
deep_green_code <deepcraft@gmail.com>
2024-03-24 22:52:14 +0100

    fix script

--- debian-script.sh
+++ debian-script.sh
@@ -4,58 +4,60 @@ green=$(tput setaf 2)
 yellow=$(tput setaf 3)
 reset=$(tput sgr0)
 
+# TODO check OS
+
 set -o allexport
 source .env
 set +o allexport
 
-if [ "$LOG" ] then
-    echo "---------------------------------------------------">>$LOGFILE
-    echo "---------------------------">>$LOGFILE
-    date>>$LOGFILE
-    echo "---------------------------">>$LOGFILE
+if [ "$LOG" ] ; then
+    echo "---------------------------------------------------">>$LOGFILE;
+    echo "---------------------------">>$LOGFILE;
+    date>>$LOGFILE;
+    echo "---------------------------">>$LOGFILE;
 fi
 
-if [ "$UPDATE" ] then
-    if [ "$LOG" ] then
-        echo "${yellow}==>${reset} apt update...">>$LOGFILE
+if [ "$UPDATE" ] ; then
+    if [ "$LOG" ] ; then
+        echo "${yellow}==>${reset} apt update...">>$LOGFILE;
     fi
-    sudo apt update
+    sudo apt update;
 fi
 
-if [ "$FULLUPGRADE" ] then
-    if [ "$LOG" ] then
-        echo "${yellow}==>${reset} Running full-upgrade...">>$LOGFILE
+if [ "$FULLUPGRADE" ] ; then
+    if [ "$LOG" ] ; then
+        echo "${yellow}==>${reset} Running full-upgrade...">>$LOGFILE;
     fi
-    sudo apt full-upgrade -y
+    sudo apt full-upgrade -y;
 fi
 
-if [ "$AUTOCLEAN" ] then
-    if [ "$LOG" ] then
-        echo "${green}==>${reset} Cleaning...">>$LOGFILE
+if [ "$AUTOCLEAN" ] ; then
+    if [ "$LOG" ] ; then
+        echo "${green}==>${reset} Cleaning...">>$LOGFILE;
     fi
-    sudo apt autoclean -y
+    sudo apt autoclean -y;
 fi
-if [ "$AUTOREMOVE" ] then
-    if [ "$LOG" ] then
-        echo "${green}==>${reset} Cleaning...">>$LOGFILE
+if [ "$AUTOREMOVE" ] ; then
+    if [ "$LOG" ] ; then
+        echo "${green}==>${reset} Cleaning...">>$LOGFILE;
     fi
-    sudo apt autoremove -y
+    sudo apt autoremove -y;
 fi
 
 # Antivirus
-if [ "$VIRUSCAN" ] then
-    if [ "$LOG" ] then
-        echo "${yellow}==>${reset} Start scan...">>$LOGFILE
+if [ "$VIRUSCAN" ] ; then
+    if [ "$LOG" ] ; then
+        echo "${yellow}==>${reset} Start scan...">>$LOGFILE;
     fi
-    sudo clamscan --infected --remove --recursive $SCANFOLDER
-    if [ "$LOG" ] then
-        echo "${yellow}==>${reset} Start scan...">>$LOGFILE
+    sudo clamscan --infected --remove --recursive $SCANFOLDER;
+    if [ "$LOG" ] ; then
+        echo "${yellow}==>${reset} Scan finished !">>$LOGFILE;
     fi
 fi
 
 
-if [ "$LOG" ] then
-    echo "${green}==>${reset} All Updates & Cleanups Finished">>$LOGFILE
+if [ "$LOG" ] ; then
+    echo "${green}==>${reset} All Updates & Cleanups Finished">>$LOGFILE;
 fi
 
 exit 0
